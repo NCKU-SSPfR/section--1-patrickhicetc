630df775980e82f902deaf9fa3dbe8785fe57a06
patrickhicetc <spp0515@gmail.com>
2025-03-10 21:46:56 +0800

    refactor: refact _load_maze_from_json function

--- DarkMaze/backend/src/game/judge.py
+++ DarkMaze/backend/src/game/judge.py
@@ -2,6 +2,10 @@ import numpy as np
 import re
 import json
 
+ARRIVE_AT_DESTINATION_HEALTH = 666
+GAME_OVER_HEALTH = 0
+OBSTACLE_VALUE = 1
+
 def _parse_map(map_string, map_size, reversal_nodes=[]):
     width, height = map_size
     filtered_chars = re.sub(r'[^a-zA-Z]', '', map_string)
@@ -23,7 +27,7 @@ def _parse_map(map_string, map_size, reversal_nodes=[]):
     
     for x, y in reversal_nodes:
         if 0 <= x < height and 0 <= y < width:
-            grid[y, x] = 1 - grid[y, x]
+            grid[y, x] ^= 1
     
     return grid
 
@@ -31,21 +35,12 @@ def _load_maze_from_json(maze_level_name):
     with open("./src/game/maze_level/" + maze_level_name + ".json", 'r', encoding='utf-8') as f:
         data = json.load(f)
     
-    maze_level_name = data.get("maze_level_name", "Unknown Level")
-    map_size = tuple(data.get("map_size", [10, 10]))
-    starting_position = tuple(data.get("starting_position", [0, 0]))
-    end_position = tuple(data.get("end_position", [0, 0]))
-    map_string = data.get("map", "")
-    reversal_nodes = data.get("reversal_node", [])
-    
-    parsed_map = _parse_map(map_string, map_size, reversal_nodes)
-    
     return {
-        "maze_level_name": maze_level_name,
-        "map_size": map_size,
-        "starting_position": starting_position,
-        "end_position": end_position,
-        "map": parsed_map
+        "maze_level_name": data.get("maze_level_name", "Unknown Level"),
+        "map_size": tuple(data.get("map_size", [10, 10])),
+        "starting_position": tuple(data.get("starting_position", [0, 0])),
+        "end_position": tuple(data.get("end_position", [0, 0])),
+        "map": _parse_map(data.get("map", ""), tuple(data.get("map_size", [10, 10])), data.get("reversal_node", []))
     }
 
 def hit_obstacle(position, maze_level_name):
@@ -56,13 +51,13 @@ def hit_obstacle(position, maze_level_name):
     # Check if the position is within the bounds of the grid
     if 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1]:
         # Return True if there's an obstacle (1) at the position, False if free space (0)
-        return grid[y, x] == 1
+        return grid[y, x] == OBSTACLE_VALUE
     else:
         # Position is out of bounds
         return True
 
 def game_over(health):
-    return health in {0, 666}
+    return health in {GAME_OVER_HEALTH, ARRIVE_AT_DESTINATION_HEALTH}
 
 def arrive_at_destination(maze_level_name, current_position):
     with open("./src/game/maze_level/" + maze_level_name + ".json", 'r', encoding='utf-8') as f:
